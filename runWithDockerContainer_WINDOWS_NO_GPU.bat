11fad4be3dac6305e8383bcb1e08839f6142b3c6
AlexWorland <59108991+AlexWorland@users.noreply.github.com>
2022-05-10 18:18:41 -0700

    Add versions of windows script for GPU and NO GPU

---
+++ runWithDockerContainer_WINDOWS_NO_GPU.bat
@@ -0,0 +1,2 @@
+docker build -t "animal-crossing-binary-training" .
+docker run --rm -v %cd%:/app/ -v %cd%\..\animal-crossing-loader\:/animal-crossing-loader/ -w /app/ animal-crossing-binary-training python3 train-model.py 
