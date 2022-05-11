5955cf6032d5deaafeea2d51025e6e5ffa9d6c81
AlexWorland <59108991+AlexWorland@users.noreply.github.com>
2022-05-10 18:20:27 -0700

    Removed redundant file

--- runWithDockerContainer_WINDOWS.bat
+++
@@ -1,2 +0,0 @@
-docker build -t "animal-crossing-binary-training" .
-docker run --gpus all --rm -v %cd%:/app/ -v %cd%\..\animal-crossing-loader\:/animal-crossing-loader/ -w /app/ animal-crossing-binary-training python3 train-model.py 
